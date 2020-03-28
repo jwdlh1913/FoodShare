03d6b995b22537314e6c8e70b5d203a134c35ee5
Deng375 <707078107@qq.com>
2020-03-29 00:10:00 +0800

    第一次提交menu

--- food-share/src/pages/Menu/index.js
+++ food-share/src/pages/Menu/index.js
@@ -1,6 +1,6 @@
 import React,{ Component }  from 'react'
 import MenuApi from '../../api/menu'
-import {message ,Table ,Pagination} from 'antd'
+import {message ,Table ,Pagination,Button} from 'antd'
 import style from './goodsList.module.less'
 
 class GoodsList extends Component{
@@ -12,14 +12,25 @@ class GoodsList extends Component{
     columns:[
       {title:'id',dataIndex:'_id',key:'_id',fixed:"left",width:80},
       {title:'菜名',dataIndex:'title',key:'title',width:80},
-      {title:'简介',dataIndex:'tags',key:'tags',width:80},
-      {title:'来源',dataIndex:'imtro',key:'imtro',width:80,height:80},
-      {title:'类别',dataIndex:'kind',key:'kind',width:80},
+      {title:'类别',dataIndex:'kind',key:'kind',width:80,render(kind){
+        console.log(kind); 
+        return (
+          <span>{kind? kind.kindName:"暂无类别"}</span>
+        ) 
+      }},
       {title:'主材',dataIndex:'ingredients',key:'ingredients',width:80},
-      {title:'辅料',dataIndex:'burden',key:'burden',width:80},
-      {title:'主图',dataIndex:'albums',key:'albums',width:80},
+      {title:'主图',dataIndex:'albums',key:'albums',width:80,render(albums){
+        return (<img width='80' height='80' alt='' src={albums} />)
+      }},
       {title:'用户',dataIndex:'userId',key:'userId',width:80},
-      {title:'操作',key:'action',width:80,fixed:'right'}
+      {title:'操作',key:'action',width:80,fixed:'right',render:(recode)=>{
+        return (
+          <div>
+            <Button type="danger" size='small'>删除</Button>
+            <Button type="primary" size='small'>修改</Button>
+          </div>
+        )
+      }}
     ]
   }
   componentDidMount(){//组件挂载完获取数据
