11daba914ab44876e3601245dec81dbfe8afef27
Deng375 <707078107@qq.com>
2020-03-30 23:15:05 +0800

    添加修改功能

--- food-share/src/pages/Menu/goodsUpdate.js
+++ food-share/src/pages/Menu/goodsUpdate.js
@@ -1,11 +1,11 @@
 import React,{ Component }  from 'react'
 import uploadApi from '../../api/upload'
 import menuApi from '../../api/menu'
-import {Card,Input,Button,Form,Upload,message,Select} from 'antd'
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
 import menuTypesApi from '../../api/menuTypes'
+import {Card,Input,Button,Form,Upload,message,Select,Spin} from 'antd'
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
 
-class GoodsAdd extends Component{
+class GoodsUpdate extends Component{
   state = {
     "title":'菜名',
     "tags":"简介",
@@ -16,20 +16,29 @@ class GoodsAdd extends Component{
     "albums":"",
     "types":[],
     loading: false,
+    spinning:false,
   }
-  addGoods = async() =>{
+  updateGoods = async() =>{//点击添加按钮
+    let {id} = this.props.match.params//获取要修改的商品id
     if(!this.state.albums){return message.info('请先上传图片')}
-    let {code,msg} = await menuApi.add(this.state)
+    let {code,msg} = await menuApi.update(id,this.state)
     if(code){return message.error(msg)}
-    message.success('添加成功,正在跳转到菜谱列表')
     this.props.history.replace('/admin/menulist')
+    
+    // let {code,msg} = await menuApi.add(this.state)
+    // if(code){return message.error(msg)}
+    // message.success('添加成功,正在跳转到菜谱列表')
+    // this.props.history.replace('/admin/menulist')
     
   }
   
-  async componentDidMount(){
-    let {list} = await menuTypesApi.typeListAll()
-    this.setState({types:list})
-    console.log(list);
+  async componentDidMount(){//组件挂载完
+    let {id} = this.props.match.params//获取要修改的菜品id
+    let {result} = await menuApi.findOne(id)//获取id对应的菜品信息
+    console.log(result);
+    
+    let {list} = await menuTypesApi.typeListAll()//获取types类型数组
+    this.setState({types:list,...result[0]})   
     
   }
   
@@ -59,11 +68,13 @@ class GoodsAdd extends Component{
   }
   //表单配置
   selectChange = value => {
+    console.log(value);
+    
     this.setState({kind:value});
   }
   //渲染列表
   render(){
-    let {albums,types} = this.state
+    let {albums,types,title,tags,ingredients,burden,kind,imtro} = this.state
     //图片配置项
     const uploadButton = (
       <div>
@@ -86,18 +97,8 @@ class GoodsAdd extends Component{
       headers: {
         authorization: 'authorization-text',
       },
-      // onChange(info) {
-      //   if (info.file.status !== 'uploading') {
-      //     console.log(info.file, info.fileList);
-      //   }
-      //   if (info.file.status === 'done') {
-      //     message.success(`${info.file.name} file uploaded successfully`);
-      //   } else if (info.file.status === 'error') {
-      //     message.error(`${info.file.name} file upload failed.`);
-      //   }
-      // },
     };
-    //表单配置项
+    /* --------------------------表单配置项--------------------------*/
     const layout = {
       labelCol: { span: 2 },
       wrapperCol: { span: 16 },
@@ -107,31 +108,32 @@ class GoodsAdd extends Component{
     }
     //类别配置项
     const { Option } = Select;
-    // const residences = this.typesItem()//类别    
+
+    /* --------------------------渲染标签--------------------------*/
     return(
       <Card title="菜品添加">
         <Form {...layout} name="nest-messages" validateMessages={validateMessages}>
       {/* 菜名 */}
-      <Form.Item name={['title', 'name']} label="菜名" rules={[{ required: true }]}>
-        <Input onChange={(e)=>{
+      <Form.Item label="菜名" rules={[{ required: true }]}>
+        <Input value={title} onChange={(e)=>{
           this.setState({title:e.target.value})
         }} />
       </Form.Item>
       {/* 简介 */}
-      <Form.Item name={['tags']} label="简介" rules={[{ required: true }]}>
-        <Input onChange={(e)=>{
+      <Form.Item label="简介" rules={[{ required: true }]}>
+        <Input value={tags} onChange={(e)=>{
           this.setState({tags:e.target.value})
         }} />
       </Form.Item>
       {/* 来源 */}
-      <Form.Item name={['imtro']} label="来源" rules={[{ required: true }]}>
-        <Input onChange={(e)=>{
+      <Form.Item label="来源" rules={[{ required: true }]}>
+        <Input value={imtro} onChange={(e)=>{
           this.setState({imtro:e.target.value})
         }} />
       </Form.Item>
       {/* 分类 */}
-      <Form.Item name={['kind']} label="类别" rules={[{ required: true }]}>
-      <Select placeholder='请选择类别' 
+      <Form.Item label="类别" rules={[{ required: true }]}>
+      <Select value={kind.menutypesName} placeholder='请选择类别' 
         style={{ width: 120 }} 
         onChange={this.selectChange}>
           {types.map((item)=>{
@@ -141,31 +143,15 @@ class GoodsAdd extends Component{
           })}
       </Select>
       </Form.Item>
-
-      {/* <Form.Item
-        name="kind"
-        label="类别"
-        value={kind}
-        rules={[
-          { type: 'array', required: true, message: '请选择类别' },
-        ]}
-        onChange={(e)=>{
-          console.log(e.target);
-          
-          this.setState({kind:e.target.value})
-        }} 
-      >
-        <Cascader options={residences} />
-      </Form.Item> */}
       {/* 主材 */}
-      <Form.Item name={['ingredients']} label="主材" rules={[{ required: true }]}>
-        <Input onChange={(e)=>{
+      <Form.Item label="主材" rules={[{ required: true }]}>
+        <Input value={ingredients} onChange={(e)=>{
           this.setState({ingredients:e.target.value})
         }}/>
       </Form.Item>
       {/* 辅料 */}
-      <Form.Item name={['burden']} label="辅料" rules={[{ required: true }]}>
-        <Input onChange={(e)=>{
+      <Form.Item label="辅料" rules={[{ required: true }]}>
+        <Input value={burden} onChange={(e)=>{
           this.setState({burden:e.target.value})
         }} />
       </Form.Item>
@@ -174,14 +160,19 @@ class GoodsAdd extends Component{
         listType="picture-card"
         className="avatar-uploader"
         beforeUpload={this.beforeUpload}
-        onChange={this.handleChange}
-      >
+        onChange={this.handleChange}>
         {albums ? <img src={"http://localhost:3000"+albums} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
       </Upload>
       {/* 添加 */}
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit" onClick={this.addGoods}>
-          添加
+        <Button type="warn" style={{marginRight:5}} htmlType="submit" 
+                onClick={()=>{
+                  this.props.history.go(-1)
+                }}>
+          返回
+        </Button>
+        <Button type="primary" htmlType="submit" onClick={this.updateGoods}>
+          修改
         </Button>
       </Form.Item>
     </Form>
@@ -189,4 +180,4 @@ class GoodsAdd extends Component{
     )
   }
 }
-export default GoodsAdd
+export default GoodsUpdate
